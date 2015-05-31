fdcbb5aebb2fc0c9ba8ce74e330f221a1aac1521
Chris Pryby <chris.pryby@udacity.com>
2015-05-31 16:57:46 -0400

    add(custom scripts)
    adds kitty, curse, and define scripts

---
+++ scripts/urbandictionary.js
@@ -0,0 +1,22 @@
+module.exports = function (robot) {
+
+  var crawl = function (error, response_code, body) {
+    if (error) {
+      return 'No definition found :(';
+    } else {
+      json = JSON.parse(body);
+      return json.list[0].definition;
+    }
+  };
+
+  var request = require('request');
+  var json;
+  var website = 'http://api.urbandictionary.com/v0/define?term=';
+
+  robot.respond(/define( me)? (.*)/i, function (msg) {
+    request.get({ url: website+msg }, function (error, response_code, body) {
+      msg.send(crawl(error, response_code, body));
+    });
+  });
+
+};
