9b356228c9b1b6ded746a36736f3a85d37b7da03
Chris Pryby <chris.pryby@udacity.com>
2015-07-20 14:19:00 -0400

    fix(dieroll)

--- scripts/dieroll.js
+++ scripts/dieroll.js
@@ -2,14 +2,14 @@ module.exports = function (robot) {
 
   var roll = function (numDice, numSides) {
     var total = 0;
-    for( int i = 0; i < numDice; i++ ) {
+    for( var i = 0; i < numDice; i++ ) {
       total += Math.floor((Math.random() * numSides) + 1);
     }
-    return total;
+    return total.toString();
   };
 
   robot.respond(/roll( me)? (.*)d(.*)/i, function (res) {
-      res.send(roll(res.match[2], res.match[3]));
+      res.send(roll(parseInt(res.match[2]), parseInt(res.match[3])));
   });
 
 };
